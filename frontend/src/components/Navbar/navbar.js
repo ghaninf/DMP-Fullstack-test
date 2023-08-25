0d951ce7054a1bd123c42e80794eaad13dd2d014
abcd123 <ghaninf@gmail.com>
2023-08-25 18:27:15 +0700

    add logout

--- frontend/src/components/Navbar/navbar.js
+++ frontend/src/components/Navbar/navbar.js
@@ -1,15 +1,29 @@
-export default function Navbar(props) {
+import { useNavigate } from "react-router-dom"
+import { AuthService } from "../../services"
+import Button from "../Button/button"
 
+export default function Navbar(props) {
+  const navigate = useNavigate();
   const pages = {
     jobs: true,
     job: true
   }
 
+  const logout = () => {
+    AuthService.logout();
+    navigate('/login')
+  }
+
   if (pages[props.page[1]]) {
     return(
       <div className="relative">
-        <div className="px-2 py-3 bg-sky-600">
+        <div className="relative px-2 py-3 flex flex-row justify-between bg-sky-600">
           <h1 className="text-xl font-bold text-white">GitHub <span className="font-thin">Jobs</span></h1>
+          <Button
+            text="Logout"
+            typeColor={'secondary'}
+            onClick={logout}
+          />
         </div>
       </div>
     )
