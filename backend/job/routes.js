d70aa9e490db2608078088c8a5623d0b6eb73040
abcd123 <ghaninf@gmail.com>
2023-08-25 17:52:10 +0700

    add auth on routes

--- backend/job/routes.js
+++ backend/job/routes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 const controller = require('./controllers');
+const { auth } = require('../middleware')
 
 router.get(
   '/list',
+  auth.authenticate(),
   controller.getList
 );
 
 router.get(
   '/:id',
+  auth.authenticate(),
   controller.get
 )
 
---
+++ backend/middleware/authentication.js
@@ -0,0 +1,23 @@
+const Token = require('../libs/token')
+
+const Authentication = {
+  authenticate() {
+    return (req, res, next) => {
+      const authToken = req.headers.authorization?.replace('Bearer ', '')
+      if (authToken) {
+        try {
+          const auth = Token.verifyJWTToken(authToken)
+          req.account = auth
+
+          return next()
+        } catch (error) {
+          return res.status(401).json({ message: 'Unauthorized access.' })
+        }
+      }
+
+      return res.status(401).json({ message: 'Unauthorized access.' })
+    }
+  }
+}
+
+module.exports = Authentication
